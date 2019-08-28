84fba65229d55342d5ffdc13cfc858aa214f0262
Bhadouria <dbhadour@iu.edu>
2019-08-28 17:14:54 -0400

    Almost done 01 Introduction

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('\033[''32mHello, My name is Devarshi Bhadouria')
+print('\033[''33m''1) My favorite game is The last of us from Naughty Dogs')
+print('\033[34m''2) I don''t know how to work github and vscode well which is affecting how fast I get the assignment')
+print('\033[35m''3) I am excited about learning new stuff and making new friends')
+print('\033[91m''4) 11991289')
+print('\033[36m''5) https://github.com/dbhadour')
