18351815121e6460c4a6d161fa4cc18514c139eb
dbhadour <deviu2022@gmail.com>
2019-08-29 14:22:28 -0400

    Final 01Introduction

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 import utils
-
+from colorama import Fore, Back,Style
 utils.check_version((3,7))
 utils.clear()
 
-print('\033[''32mHello, My name is Devarshi Bhadouria')
+print(Back.WHITE + '\033[''32mHello, My name is Devarshi Bhadouria')
 print('\033[''33m''1) My favorite game is The last of us from Naughty Dogs')
-print('\033[34m''2) I don''t know how to work github and vscode well which is affecting how fast I get the assignment')
+print('\033[34m''2) I don''t know how to work github and vscode well which is affecting how fast I able to make progress on the assignment')
 print('\033[35m''3) I am excited about learning new stuff and making new friends')
 print('\033[91m''4) 11991289')
 print('\033[36m''5) https://github.com/dbhadour')
+
+
